e7f270eada21274976a1f40733f77dea8dce40c2
smsissoko <smsissoko19@gmail.com>
2020-01-27 07:59:56 +0000

    Project Init

---
+++ src/main/java/ml/malisoft/telimanProject/TelimanProjectApplication.java
@@ -0,0 +1,13 @@
+package ml.malisoft.telimanProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelimanProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelimanProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ml/malisoft/telimanProject/TelimanProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ml.malisoft.telimanProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelimanProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
